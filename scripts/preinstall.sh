d06b5277c49c999c0d6ea41094847c3bd712f50d
jyotirajsingh <jyotirajsingh3@gmail.com>
2018-11-30 23:06:14 +0530

    githook check

---
+++ scripts/preinstall.sh
@@ -0,0 +1,7 @@
+echo 'preinstall script called'
+
+git init
+chmod +x scripts/pre-commit
+chmod +x scripts/commit-msg
+ln -s -f ../../scripts/pre-commit .git/hooks/pre-commit
+ln -s -f ../../scripts/commit-msg .git/hooks/commit-msg
